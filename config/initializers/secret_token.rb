26cdec65e7c64015412d6dfe70d713e04b555b97
Lcn23 <lucianocagnoli@gmail.com>
2016-06-13 17:26:03 -0300

    Commit 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoAppLcn::Application.config.secret_token = 'eb9bab2c1663db4ca05318860791f13147b3854040fadffc41272af240b465858c6fd4afb8297642480ab9ad86396f754fe4c2a3a0d4fbcc894586ac5b880a28'
